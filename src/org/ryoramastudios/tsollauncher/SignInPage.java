d92bc500eb087109935e1bc0199857ff7fe0b6b1
KingRealzYT <jonathansanie@gmail.com>
2021-05-01 22:50:41 -0500

    Adding remember me thing

--- src/org/ryoramastudios/tsollauncher/SignInPage.java
+++ src/org/ryoramastudios/tsollauncher/SignInPage.java
@@ -13,59 +13,70 @@ public class SignInPage {
     private JCheckBox iAgreeToTheCheckBox;
     private JButton signInButton;
     private JCheckBox showPassword;
+    private JCheckBox rememberMe;
     public static JFrame frame = new JFrame("TSOL Launcher");
     public static boolean signIn = false;
 
     public SignInPage() {
-        signInButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String userText;
-                String pwdText;
-                userText = email.getText();
-                pwdText = password.getText();
-                if (iAgreeToTheCheckBox.isSelected()) {
-                    if (userText.equalsIgnoreCase("realz@gmail.com") && pwdText.equals("pogs1")) {
-                        JOptionPane.showMessageDialog(null, "Login Successful");
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Login Unsuccessful, Invalid Username or Password");
+            signInButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String userText;
+                    String pwdText;
+                    userText = email.getText();
+                    pwdText = password.getText();
+                    if (iAgreeToTheCheckBox.isSelected()) {
+                        if (userText.equalsIgnoreCase("realz@gmail.com") && pwdText.equals("pogs1")) {
+                            JOptionPane.showMessageDialog(null, "Login Successful");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Login Unsuccessful, Invalid Username or Password");
+                        }
+                    } else if (!iAgreeToTheCheckBox.isSelected()) {
+                        JOptionPane.showMessageDialog(null, "Agree to the TOS!");
                     }
-                } else if (!iAgreeToTheCheckBox.isSelected()) {
-                    JOptionPane.showMessageDialog(null, "Agree to the TOS!");
                 }
-            }
-        });
+            });
 
-        showPassword.addActionListener(new ActionListener() {
+            showPassword.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (showPassword.isSelected()) {
+                        password.setEchoChar((char) 0);
+                    } else {
+                        password.setEchoChar('*');
+                    }
+                }
+            });
+        rememberMe.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if (showPassword.isSelected()) {
-                    password.setEchoChar((char) 0);
-                } else {
-                    password.setEchoChar('*');
+                if (rememberMe.isSelected()) {
+                    signIn = true;
                 }
             }
         });
     }
 
-    public static void main(String[] args) {
-        frame.setContentPane(new SignInPage().loginPage);
-        frame.pack();
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                int confirm = JOptionPane.showConfirmDialog(frame, "Are you sure you want to exit?",
-                        "Exit?", JOptionPane.YES_NO_OPTION);
+        public static void main (String[]args) {
+            if (!signIn) {
+                frame.setContentPane(new SignInPage().loginPage);
+                frame.pack();
+                frame.setVisible(true);
+                frame.setLocationRelativeTo(null);
+                frame.setResizable(false);
+                frame.addWindowListener(new WindowAdapter() {
+                    @Override
+                    public void windowClosing(WindowEvent e) {
+                        int confirm = JOptionPane.showConfirmDialog(frame, "Are you sure you want to exit?",
+                                "Exit?", JOptionPane.YES_NO_OPTION);
 
-                if (confirm == JOptionPane.YES_OPTION) {
-                    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                } else {
-                    frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-                }
+                        if (confirm == JOptionPane.YES_OPTION) {
+                            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                        } else {
+                            frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+                        }
+                    }
+                });
             }
-        });
-    }
+        }
 }
